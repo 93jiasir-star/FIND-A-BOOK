35cd3f78a8439311bee7358e805b68628048d0ae
佳Sir <93jiasir@gmail.com>
2026-08-03 22:40:33 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,5 @@
 const CACHE = 'find-a-book-v29';
-const ASSETS = ['./index.html', './style.css?v=28', './app.js?v=28', './manifest.webmanifest', './icon.svg', './xuan-paper.png'];
-
-self.addEventListener('install', event => event.waitUntil(
-  caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
-));
-
-self.addEventListener('activate', event => event.waitUntil(
-  caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())
-));
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== location.origin) return;
-  if (event.request.mode === 'navigate') {
-    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
-});
+const ASSETS = ['./', './index.html', './style.css?v=28', './app.js?v=28', './manifest.webmanifest', './icon.png', './assets/xuan-paper.png'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => { if (event.request.method === 'GET' && new URL(event.request.url).origin === location.origin) event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))); });
