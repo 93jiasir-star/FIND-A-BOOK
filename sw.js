ada28abc493910deb8d45b41058b30878f2146c1
佳Sir <93jiasir@gmail.com>
2026-08-03 23:19:16 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'find-a-book-v29';
+const CACHE = 'find-a-book-v30';
 const ASSETS = ['./', './index.html', './style.css?v=28', './app.js?v=28', './manifest.webmanifest', './icon.png', './assets/xuan-paper.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
